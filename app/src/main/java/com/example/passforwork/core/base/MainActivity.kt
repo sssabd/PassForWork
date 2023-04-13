3fa6fe13cb702ed78fbd2310ae5ceb442b4d5316
Abdusalyamova S <dimka.0203.inbox.ru@gmail.com>
2023-04-13 10:16:36 +0300

    Added layouts for screens "object settings" and "main screen"

--- app/src/main/java/com/example/passforwork/core/base/MainActivity.kt
+++ app/src/main/java/com/example/passforwork/core/base/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.passforwork.core.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import com.example.passforwork.R
+import com.example.passforwork.features.registration.presentation.RegistrationFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_registration)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, RegistrationFragment())
+                .commitNow()
+        }
     }
 }
